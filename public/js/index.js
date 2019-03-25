db778333e90fe388ae4bb94e3442cd272265a893
Eric <elymberopoulos@luc.edu>
2019-03-25 12:44:54 -0500

    npm scripts updated for heroku. also basic connection now established between server and client

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+//Webpack will bundle these requires into a JS bundle for index.html to use
+require('../css/index.css');
+const $ = require('jquery');
+
+//establish connection to server with a socket
+var socket = io();
+
+
+socket.on('connect', () => {
+    console.log("Connected to server.");
+});
+
+
--- server/server.js
+++ server/server.js
@@ -5,24 +5,22 @@ const express = require('express');
 const socketIO = require('socket.io');
 //This port will guarantee that when deployed an available port will be found
 const port = process.env.PORT || 8080;
-
-//Save a path to the index.html file
-const mainHTMLPath = path.join(__dirname, '../html');
+//set public directory path for express to serve up the files
+const publicDir = path.join(__dirname, '../public');
 
 //Create server object and pass it to socketIO so sockets run on server
 var app = express();
 var server = http.createServer(app);
-var io = socketIO(server);
-
 
 //Express will use static middleware
-app.use(express.static(mainHTMLPath));
+app.use(express.static(publicDir));
 
 //Have server listen on specified port
 server.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
 
+var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('new user connection...');
 });
