9972d865caaf1e0ef18224873b00a637728c7ada
Vestigialearth <38117731+Vestigialearth@users.noreply.github.com>
2019-04-02 13:49:51 -0500

    Add files via upload

---
+++ Nes-Backend/webpack.config.js
@@ -0,0 +1,24 @@
+var path = require("path");
+var pkg = require("./package.json");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: "./lib/",
+    library: "NES",
+    filename: "nes.js",
+    libraryExport: "Console",
+    libraryTarget: "umd"
+  },
+  devtool: "source-map",
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
