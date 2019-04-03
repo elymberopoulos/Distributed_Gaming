6858579e44c9b13470eb370d37b653761c39b420
Eric <elymberopoulos@luc.edu>
2019-04-03 13:29:53 -0500

    emulator code working. credit to dan Shumway

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,95 @@
 require('../css/index.css');
 const $ = require('jquery');
 
-var canvas = $('#nes-canvas');
-
 //establish connection to server with a socket
 var socket = io();
+
+var canvas = document.getElementById('mainCanvas');
+var ctx = canvas.getContext('2d');
+var ctx_data = ctx.createImageData(160, 144);
+
+console.log('about to connect');
+//var socket = io.connect('localhost:3333'); //Server address goes here.
+var socket = io();
+
+//Credit to Dan Shumway for his serverboy code example
+socket.on('frame', function (data) {
+    for (var i = 0; i < data.length; i++) {
+        ctx_data.data[i] = data[i];
+    }
+
+    ctx.putImageData(ctx_data, 0, 0);
+});
+
+var audioContext = new AudioContext();
+var frames = {};
+socket.on('audio', function (data) {
+
+    //data needs to be copied to an array.
+    var buffers = {
+        left: [],
+        right: []
+    };
+    /* for (let i = 0; i < data.length; i+=2) {*/
+    /* buffers.left.push(data[i] || 0);*/
+    /* buffers.right.push(data[i+1] || 0);*/
+    /* }*/
+
+    var buffer = audioContext.createBuffer(1, data.length, 44150.56842105263);
+    buffer.getChannelData(0).set(data);
+    //buffer.getChannelData(1).set(buffers.right);
+
+    var source = audioContext.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioContext.destination);
+    source.start();
+});
+
+
+window.onkeydown = function (e) {
+    var keys = {
+        "37": "left",
+        "39": "right",
+        "38": "up",
+        "40": "down",
+        "90": "a",
+        "88": "b",
+        "13": "start",
+        "32": "select"
+    };
+
+    if (keys[e.keyCode] != undefined) {
+        socket.emit('keydown', { key: keys[e.keyCode] });
+    } else {
+        if (e.keyCode === 27) {
+            socket.emit('restart', {});
+        }
+    }
+}
+
+window.onkeyup = function (e) {
+    var keys = {
+        "37": "left",
+        "39": "right",
+        "38": "up",
+        "40": "down",
+        "90": "a",
+        "88": "b",
+        "13": "start",
+        "32": "select"
+    }
+    if (keys[e.keyCode]) {
+        socket.emit('keyup', { key: keys[e.keyCode] });
+    }
+}
+
+
+
+
+
+
+
+
 
 
 socket.on('connect', () => {
@@ -14,10 +99,6 @@ socket.on('connect', () => {
 
 socket.on('userDisconnect', (currentUsers) => {
     console.log(`A user disconnected ${currentUsers} remaining`);
-});
-
-socket.on('frame', (frame) => {
-    console.log(`Frame received: ${frame}`);
 });
 
 
--- server/server.js
+++ server/server.js
@@ -5,22 +5,13 @@ const express = require('express');
 const socketIO = require('socket.io');
 const port = process.env.PORT || 8080;//This port will guarantee that when deployed an available port will be found
 const publicDir = path.join(__dirname, '../public');//set public directory path for express to serve up the files
-const emulator = require('./emulator/nesEmulator');
+const gameboy = require('serverboy');
 
 //Create server object and pass it to socketIO so sockets run on server
 var app = express();
 var server = http.createServer(app);
-
 app.use(express.static(publicDir));//Express will use static middleware
-
 var currentUsers = 0;//variable to track the total current users on the server
-
-//Emulator Variables
-var game = emulator.Emulator();//Main game
-var canvas = emulator.canvas;//Canvas that server displays to
-var rom = emulator.rom_data;
-// game.nes_init
-
 
 //Have server listen on specified port
 server.listen(port, () => {
@@ -28,6 +19,95 @@ server.listen(port, () => {
 });
 
 var io = socketIO(server);
+
+var currentScreen;
+
+
+//Credit to Dan Shumway for his serverboy code example
+function loadROM(file_path) {
+
+    var rom = fs.readFileSync(file_path);
+
+    //start the rom.
+    var gameboy_instance = new gameboy();
+    gameboy_instance.loadRom(rom);
+
+    // var io; //Handle streaming.
+    var keysToPress = []; //What keys we want pressed.
+
+    var start_io = function () {
+
+        io.on('connection', function (socket) {
+            console.log('connection happened');
+            //Logic for handeling a new connection here.
+
+            //The new connection can send commands.
+            socket.on('keydown', function (data) {
+                var index = keysToPress.indexOf(data.key);
+                if (index === -1) {
+                    keysToPress.push(data.key);
+                }
+            });
+
+            socket.on('keyup', function (data) {
+                var index = keysToPress.indexOf(data.key);
+                if (index !== -1) {
+                    keysToPress.splice(index, 1);
+                }
+            });
+
+            socket.on('restart', function (data) {
+                gameboy_instance.loadROM(rom);
+            });
+
+        });
+    };
+
+    //Handle doing a single frame.
+    //You want to basically time this at about 60fps.
+    var frames = 0; var lastFrame = undefined; var currentFrame = undefined;
+    var audioLoop = [];
+    var emulatorLoop = function () {
+        var start = process.hrtime();
+
+        gameboy_instance.pressKeys(keysToPress);
+        currentScreen = gameboy_instance.doFrame();
+
+        // let currentAudio = gameboy_instance.getAudio();
+
+        //Compress into mono - literally throw away half the frames
+        // for (let i = 0; i < 705; i += 2) {
+        //     audioLoop.push(currentAudio[i]);
+        // }
+
+        frames++;
+        if (frames % 10 === 0) { //Output every 10th frame.
+            if (io) {
+                console.log(frames);
+
+                io.emit('frame', currentScreen);
+                io.emit('memory', gameboy_instance.getMemory());
+                // io.emit('audio', audioLoop);
+                audioLoop = [];
+            }
+        }
+
+        var elapsed = process.hrtime(start)[1] / 1000000;
+        setTimeout(emulatorLoop, 5); //Try and run at about 60fps.
+    };
+
+
+    start_io();
+    emulatorLoop();
+}
+
+
+console.log('starting to load rom');
+var rom = __dirname + '/emulator/rom/Pokemon.gbc';
+loadROM(rom);
+
+
+
 
 //The socket parameter is the connection between the client and the server.
 io.on('connection', (socket) => {
@@ -48,18 +128,4 @@ io.on('connection', (socket) => {
         io.emit('userDisconnect', currentUsers);
     })
 });
-
-/*
-Set an interval for the emulator frames to be emitted from the server to all connected clients.
-The clients will get the buffer array and set the canvas image with the buffer array.
-*/
-setInterval(() => {
-    canvas.toBuffer((error, bufferArray) => {
-        if (error) {
-            console.log(`Canvas.toBuffer error is: ${error}`);
-            throw error;
-        }
-        io.emit('frame', bufferArray);
-    });
-}, 50); //20 FPS
 
