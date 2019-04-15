846eb3e23f5b013925f154b97795003c54bf4a2f
pjack <pjackowski@luc.edu>
2019-04-15 16:19:04 -0500

    test/pjack merge of master and fixing tests

--- server/server.js
+++ server/server.js
@@ -5,62 +5,129 @@ const express = require('express');
 const socketIO = require('socket.io');
 const port = process.env.PORT || 8080;//This port will guarantee that when deployed an available port will be found
 const publicDir = path.join(__dirname, '../public');//set public directory path for express to serve up the files
-const emulator = require('./emulator/nesEmulator');
+const gameboy = require('serverboy');
+const randHash = require('random-hash');
 
 //Create server object and pass it to socketIO so sockets run on server
 var app = express();
 var server = http.createServer(app);
-
 app.use(express.static(publicDir));//Express will use static middleware
-
-var currentUsers = 0;//variable to track the total current users on the server
-
-//Emulator Variables
-var game = emulator.Emulator();//Main game
-var canvas = emulator.canvas;//Canvas that server displays to
-var rom = emulator.rom_data;
-// game.nes_init
-
 
 //Have server listen on specified port
 server.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
 
+//Generate random hash for peer-to-peer connection unique id's
+var randomHash = randHash.generateHash({
+    length: 5
+});
+
 var io = socketIO(server);
+var currentScreen;
+var currentUsers = 0;
 
-//The socket parameter is the connection between the client and the server.
-io.on('connection', (socket) => {
-    console.log('new user connection...');
-    currentUsers++;
-    console.log(`Total users on server ${currentUsers}`);
-    io.emit('userConnected', currentUsers)
+function adjustUserCount(value){
+    if(value === "decrement" && currentUsers > 0){
+        return currentUsers -= 1;
+    }
+    else if(value === "increment"){
+        currentUsers += 1;
+    }
+    else{
+        console.log("UserCount error.");
+    }
+}
 
-    //When the user inputs a command from the client this is where it is handled and passed to emulator running the game.
-    socket.on('command', (userCommand) => {
-        game.keyboard(userCommand);
-        console.log(`Command ${userCommand} issued to game.`);
+//Credit to Dan Shumway for his serverboy code example
+var rompath = __dirname + '/emulator/rom/Tetris.gb';
+var rom = fs.readFileSync(rompath);
+
+//start the rom.
+var gameboy_instance = new gameboy();
+gameboy_instance.loadRom(rom);
+
+// var io; //Handle streaming.
+var keysToPress = []; //What keys we want pressed.
+
+
+io.on('connection', function (socket) {
+    console.log('connection happened');
+
+    socket.on('incrementCount', (data)=>{
+        adjustUserCount('increment');
+        io.emit('checkUserCount', currentUsers);
+    });
+
+
+    //The new connection can send commands.
+    socket.on('keydown', function (data) {
+        var index = keysToPress.indexOf(data.key);
+        if (index === -1) {
+            keysToPress.push(data.key);
+        }
+    });
+
+    socket.on('keyup', function (data) {
+        var index = keysToPress.indexOf(data.key);
+        if (index !== -1) {
+            keysToPress.splice(index, 1);
+        }
     });
 
     //When a user disconnects then the total user counter is decremented and every socket is notified
     socket.on('disconnect', () => {
         console.log('user disconnected');
-        currentUsers--;
-        io.emit('userDisconnect', currentUsers);
-    })
+        io.emit('userDisconnect', adjustUserCount('decrement'));
+    
+    });
+
+    socket.on('shutDown', () => {
+        console.group('Server is Shutting Down');
+        currentUsers = 0;
+        gameboy_instance = None
+        io.close();
+    });
+
+    socket.on('restart', function (data) {
+        gameboy_instance.loadROM(rom);
+    });
+
 });
 
-/*
-Set an interval for the emulator frames to be emitted from the server to all connected clients.
-The clients will get the buffer array and set the canvas image with the buffer array.
-*/
-setInterval(() => {
-    canvas.toBuffer((error, bufferArray) => {
-        if (error) {
-            console.log(`Canvas.toBuffer error is: ${error}`);
-            throw error;
+//Handle doing a single frame.
+//You want to basically time this at about 60fps.
+var frames = 0; var lastFrame = undefined; var currentFrame = undefined;
+var audioLoop = [];
+var emulatorLoop = function () {
+
+    var start = process.hrtime();
+    gameboy_instance.pressKeys(keysToPress);
+    currentScreen = gameboy_instance.doFrame();
+
+    // let currentAudio = gameboy_instance.getAudio();
+
+    //Compress into mono - literally throw away half the frames
+    // for (let i = 0; i < 705; i += 2) {
+    //     audioLoop.push(currentAudio[i]);
+    // }
+
+    frames++;
+    if (frames % 14 === 0) { //Output every 10th frame.
+        if (io) {
+
+            io.emit('frame', currentScreen);
+            io.emit('memory', gameboy_instance.getMemory());
+
+            // io.emit('audio', audioLoop);
+            audioLoop = [];
         }
-        io.emit('frame', bufferArray);
-    });
-}, 50); //20 FPS
+    }
+
+    var elapsed = process.hrtime(start)[1] / 1000000;
+    setTimeout(emulatorLoop, 5); //Try and run at about 60fps.
+};
+
+emulatorLoop();
+
 
--- test/server/server_test.js
+++ test/server/server_test.js
@@ -7,27 +7,79 @@ var options = {
     transports: ['websocket'],
     'force new connection': true
 };
+describe('Suite of unit tests', function() {
 
-describe('Server should broadcast user count to new users', function () {
-    var client1 = io.connect(socketURL, options);
-    
-    var client2 = io.connect(socketURL, options);
-    
-    it('3rd userConnection test', function (done) {
-        var client3 = io.connect(socketURL,options);
-        client1.on('userConnected', function (userCount) {
-            assert.equal(userCount, 3);
-            client3.disconnect();
+    var socket;
+
+    beforeEach(function(done) {
+        // Setup
+        socket = io.connect(socketURL, {
+            'reconnection delay' : 0
+            , 'reopen delay' : 0
+            , 'force new connection' : true
+        });
+        socket.on('connect', function() {
+            socket.emit('incrementCount', (1));
+            done();
+        });
+        socket.on('userDisconnect', function() {
+            console.log('disconnected...');
+        });
+    });
+
+    afterEach(function(done) {
+        // Cleanup
+        if(socket.connected) {
+            console.log('disconnecting...');
+            socket.disconnect();
+        } else {
+            // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
+            console.log('no connection to break...');
+        }
+        done();
+    });
+
+    it('Server should broadcast user count to new users', function(done) {
+        socket.on('checkUserCount',function(userCount){
+            assert.equal(userCount,1);
             done();
         });
     });
 
-    it('userDisconnected test', function (done) {
-        client1.disconnect();
-        client2.on('userDisconnect', function (userCount) {
-            assert.equal(userCount, 1);
-            client2.disconnect();
+    var socket2 = io.connect(socketURL, {
+        'reconnection delay' : 0
+        , 'reopen delay' : 0
+        , 'force new connection' : true
+    });
+
+    socket2.on('connect', function(){
+        socket.emit('incrementCount', (1));
+        console.log('worked...');
+    });
+    
+    it('Server should lower count when a user disconnects', function(done){
+        socket2.disconnect();
+        
+        socket.on('userDisconnect', function(userCount){
+            if (userCount != 0){
+                assert.equal(userCount,1);
+            
+            socket.emit('shutDown');
             done();
+            }
         });
     });
 });
+
+
+    // it('userDisconnected test', function (done) {
+    //     var client1 = io.connect(socketURL);
+        
+    //     var client2 = io.connect(socketURL);
+    //     client1.disconnect();
+    //     client2.on('userDisconnect', function (userCount) {
+    //         assert.equal(userCount, 1);
+    //         client2.disconnect();
+    //         done();
+    //     });
+    // });
