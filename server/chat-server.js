02a90ffefb6620cc7b45ae3ba1435b1911e172b7
f423 <fshaheen@luc.edu>
2019-04-22 13:22:56 -0500

    added chat functionality between connected sockets, currently on a seperate server until further merges occur

--- server/chat-server.js
+++ server/chat-server.js
@@ -8,14 +8,31 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
 users = [];
-connections =[ ];
+connections =[];
 
 server.listen(process.env.PORT || 8080);
 console.log("server running ...");
 
 
 app.get('/', function(req, res){
-	//res.sendFile(__dirname + '/../public/chat.html');
+	//res.sendFile(__dirname + '/../public/index.html');
 	res.sendFile(publicDir);
 });
 
+io.sockets.on('connection', function(socket){
+	// connect
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	// disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disconnected: %s sockets connected', connections.length);
+	});
+
+	//Send Message
+	socket.on('send message', function(data){
+		console.log(data);
+		io.sockets.emit('new message', {msg: data});
+	});
+});
