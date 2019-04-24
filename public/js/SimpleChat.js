50eb9519e1437153c3b7e8348c61430464a15042
Eric <elymberopoulos@luc.edu>
2019-04-24 03:19:08 -0500

    more chat functionality added. append and clear chat

--- public/js/SimpleChat.js
+++ public/js/SimpleChat.js
@@ -1,12 +1,18 @@
 const $ = require('jquery');
 
 function simpleChat(){
+
     var chatAppend = $('#chatAppend');
     var submitChatBTN = $('#submitChat').on('click', ()=>{
         console.log('button clicked');
-        chatAppend.append("<b>"+$("#chatMessage").val()+"</b>");
-
+        chatAppend.append("<div>"+$("#chatMessage").val()+"</div>");
+        $("#chatMessage").val("");
     });
+    var clearChat = $('#emptyChat').on('click', ()=>{
+        console.log('Clear chat button clicked');
+        chatAppend.empty();
+    });
+
 }
 module.exports = {
     simpleChat
